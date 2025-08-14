88fb67194433db35ad7350ebcbd21878b454016b
ASHWATHYACHU17 <ashwathyrajkumar8@gmail.com>
2025-08-14 20:48:40 +0530

    all changes

---
+++ day9/web.js
@@ -0,0 +1,38 @@
+const con=document.getElementById('container');
+const arr=["web anime.png",
+    "img1.jpg",
+    "img2.jpg",
+    "img3.jpg",
+    "img4.jpg",
+    "img5.jpg",
+]
+
+
+
+let prev=()=>{
+     i--;
+    if(i==-1)
+    {
+        i=5;
+    }
+
+    con.style.backgroundImage=`url('${arr[i]}')`;
+
+}
+// let change=(a)=>{
+
+//  con.style.backgroundImage=`url('${arr[a]}')`;
+// }
+     
+let i=0;
+let next=()=>{
+
+     i++;
+    if(i==6)
+    {
+        i=0;
+    }
+
+    con.style.backgroundImage=`url('${arr[i]}')`;
+}
+   
